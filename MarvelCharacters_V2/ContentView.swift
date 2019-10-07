1ed3164ad01087c81a3eeb337beae034d26c0540
costaneto222 <costaneto222@gmail.com>
2019-10-07 18:37:51 -0300

    Initial Commit

---
+++ MarvelCharacters_V2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelCharacters_V2
+//
+//  Created by Francisco Costa Neto on 07/10/19.
+//  Copyright © 2019 Francisco Costa Neto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelCharacters_V2Tests/MarvelCharacters_V2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MarvelCharacters_V2Tests.swift
+//  MarvelCharacters_V2Tests
+//
+//  Created by Francisco Costa Neto on 07/10/19.
+//  Copyright © 2019 Francisco Costa Neto. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelCharacters_V2
+
+class MarvelCharacters_V2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
